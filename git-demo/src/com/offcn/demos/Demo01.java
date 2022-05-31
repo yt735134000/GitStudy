e76486b9fad818c56f4e145865c5a9afda53ab97
yt <white3384@163.com>
2022-05-31 09:54:14 +0800

    第一次提交

--- git-demo/src/com/offcn/demos/Demo01.java
+++ git-demo/src/com/offcn/demos/Demo01.java
@@ -1,7 +1,7 @@
 package com.offcn.demos;
 
-public class Demo {
+public class Demo01 {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("111111");
     }
 }
